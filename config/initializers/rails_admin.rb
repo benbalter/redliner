fb19ff080a584379b6aa24a6b341a7abcacc1b95
Ben Balter <ben.balter@github.com>
2015-05-23 13:26:03 -0400

    proper auth

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,17 +1,13 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
-
-  RailsAdmin.config do |config|
-    config.authenticate_with do
-      warden.authenticate! scope: :user
-      unless current_user.try(:admin?)
-        flash[:error] = "You are not an admin"
-        redirect_to root_path
-      end
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+    unless current_user.try(:admin?)
+      flash[:error] = "You are not an admin"
+      redirect_to "/"
     end
-    config.current_user_method(&:current_user)
   end
+  config.current_user_method(&:current_user)
 
   config.actions do
     dashboard                     # mandatory
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root "home#show"
+  get 'welcome/index'
+  root "welcome#index"
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
